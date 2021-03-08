d4a3d7c0a9d082455283dd94c9a81a77abdbf0d3
John Howard <howardjohn@google.com>
2021-03-08 15:00:16 -0800

    Add tests

--- tests/integration/security/pass_through_filter_chain_test.go
+++ tests/integration/security/pass_through_filter_chain_test.go
@@ -20,9 +20,9 @@ import (
 	"fmt"
 	"strings"
 	"testing"
-	"time"
 
 	"istio.io/istio/pkg/config/protocol"
+	"istio.io/istio/pkg/test"
 	"istio.io/istio/pkg/test/echo/common/response"
 	epb "istio.io/istio/pkg/test/echo/proto"
 	"istio.io/istio/pkg/test/framework"
@@ -86,9 +86,15 @@ func TestPassThroughFilterChain(t *testing.T) {
 						schema: protocol.TCP,
 						want:   true,
 					},
+					{
+						target: a,
+						port:   8089,
+						schema: protocol.HTTPS,
+						want:   true,
+					},
 
 					// For workload b, there is only authZ policy that allows access to port 8085 and 8087.
-					// Only request to port 8085, 8087 should be allowed.
+					// Only request to port 8085, 8087, 8089 should be allowed.
 					{
 						target: b,
 						port:   8085,
@@ -111,6 +117,18 @@ func TestPassThroughFilterChain(t *testing.T) {
 						target: b,
 						port:   8088,
 						schema: protocol.TCP,
+						want:   false,
+					},
+					{
+						target: b,
+						port:   8089,
+						schema: protocol.HTTPS,
+						want:   true,
+					},
+					{
+						target: b,
+						port:   8084,
+						schema: protocol.HTTPS,
 						want:   false,
 					},
 
@@ -140,6 +158,12 @@ func TestPassThroughFilterChain(t *testing.T) {
 						schema: protocol.TCP,
 						want:   false,
 					},
+					{
+						target: c,
+						port:   8089,
+						schema: protocol.HTTPS,
+						want:   false,
+					},
 
 					// For workload d, there is only authN policy that enables mTLS (Permissive).
 					// The request should be allowed because the a is always using plain text.
@@ -167,9 +191,15 @@ func TestPassThroughFilterChain(t *testing.T) {
 						schema: protocol.TCP,
 						want:   true,
 					},
+					{
+						target: d,
+						port:   8089,
+						schema: protocol.HTTPS,
+						want:   true,
+					},
 
 					// For workload e, there is only authN policy that disables mTLS by default and enables mTLS strict on port 8086 and 8088.
-					// The request should be denied on port 8086 and 8088.
+					// The request should be denied on port 8086, 8084, and 8088.
 					{
 						target: e,
 						port:   8085,
@@ -192,6 +222,18 @@ func TestPassThroughFilterChain(t *testing.T) {
 						target: e,
 						port:   8088,
 						schema: protocol.TCP,
+						want:   false,
+					},
+					{
+						target: e,
+						port:   8089,
+						schema: protocol.HTTPS,
+						want:   true,
+					},
+					{
+						target: e,
+						port:   8084,
+						schema: protocol.HTTPS,
 						want:   false,
 					},
 
@@ -221,14 +263,26 @@ func TestPassThroughFilterChain(t *testing.T) {
 						schema: protocol.TCP,
 						want:   true,
 					},
+					{
+						target: f,
+						port:   8089,
+						schema: protocol.HTTPS,
+						want:   false,
+					},
+					{
+						target: f,
+						port:   8084,
+						schema: protocol.HTTPS,
+						want:   true,
+					},
 				}
 				ctx.NewSubTest(fmt.Sprintf("In %s", cluster.StableName())).Run(func(ctx framework.TestContext) {
 					for _, tc := range cases {
-						name := fmt.Sprintf("A->%s:%d[%t]", tc.target.Config().Service, tc.port, tc.want)
-						a := apps.A.Match(echo.InCluster(cluster)).GetOrFail(ctx, echo.Namespace(ns.Name()))
-						from := getWorkload(a, t)
+						name := fmt.Sprintf("%s:%d[%t]", tc.target.Config().Service, tc.port, tc.want)
+						client := apps.Naked.Match(echo.InCluster(cluster)).GetOrFail(ctx, echo.Namespace(ns.Name()))
+						from := getWorkload(client, ctx)
 						// The request should be handled by the pass through filter chain.
-						host := fmt.Sprintf("%s:%d", getWorkload(tc.target, t).Address(), tc.port)
+						host := fmt.Sprintf("%s:%d", getWorkload(tc.target, ctx).Address(), tc.port)
 						request := &epb.ForwardEchoRequest{
 							Url:     fmt.Sprintf("%s://%s", tc.schema, host),
 							Message: "HelloWorld",
@@ -240,7 +294,7 @@ func TestPassThroughFilterChain(t *testing.T) {
 							},
 						}
 						ctx.NewSubTest(name).Run(func(ctx framework.TestContext) {
-							retry.UntilSuccessOrFail(t, func() error {
+							retry.UntilSuccessOrFail(ctx, func() error {
 								responses, err := from.ForwardEcho(context.TODO(), request)
 								if tc.want {
 									if err != nil {
@@ -258,12 +312,12 @@ func TestPassThroughFilterChain(t *testing.T) {
 										return nil
 									}
 
-									if err == nil || !strings.Contains(err.Error(), "EOF") {
-										return fmt.Errorf("want error EOF but got: %v", err)
+									if err == nil || (!strings.Contains(err.Error(), "connection reset by peer") && !strings.Contains(err.Error(), "EOF")) {
+										return fmt.Errorf("want error 'connection reset by peer' or 'EOF' but got: %v", err)
 									}
 								}
 								return nil
-							}, retry.Delay(250*time.Millisecond), retry.Timeout(30*time.Second))
+							}, echo.DefaultCallRetryOptions()...)
 						})
 					}
 				})
@@ -272,7 +326,7 @@ func TestPassThroughFilterChain(t *testing.T) {
 		})
 }
 
-func getWorkload(instance echo.Instance, t *testing.T) echo.Workload {
+func getWorkload(instance echo.Instance, t test.Failer) echo.Workload {
 	workloads, err := instance.Workloads()
 	if err != nil {
 		t.Fatalf(fmt.Sprintf("failed to get Subsets: %v", err))
