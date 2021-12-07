ae9ec75a1d1b0361c1b508dc79663747ab79ea9a
John Howard <howardjohn@google.com>
2021-12-07 08:34:42 -0800

    initial

--- generics/main.go
+++ generics/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"context"
-	"flag"
 	"fmt"
 	"istio.io/istio/pkg/kube"
 	"istio.io/pkg/log"
@@ -10,49 +9,38 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/client-go/rest"
-	"k8s.io/klog/v2"
 )
-
-type intOrString interface {
-	int | string
-}
-
-func test[T intOrString](x T) T {
-	return x
-}
 
 type Client struct {
 	scheme *runtime.Scheme
 	client rest.Interface
 }
 
-func Get[T runtime.Object](c *Client, name, namespace string, options metav1.GetOptions) (*T, error) {
+func Get[T any](c *Client, name, namespace string, options metav1.GetOptions) (*T, error) {
 	result := new(T)
-	rx := *result
+	x := (interface{})(result).(runtime.Object)
 	err := c.client.Get().
 		Namespace(namespace).
 		Resource("deployments").
 		Name(name).
 		VersionedParams(&options, runtime.NewParameterCodec(c.scheme)).
 		Do(context.Background()).
-		Into(rx)
+		Into(x)
 	return result, err
 }
 
 func main() {
-	fs := &flag.FlagSet{}
-	fs.Set("v", "6")
-	klog.InitFlags(fs)
-	// --v= flag of klog.
 	restConfig, err := kube.DefaultRestConfig("", "")
 	if err != nil {
 		log.Fatal(err)
 	}
-	k, err := kube.NewExtendedClient(kube.NewClientConfigForRestConfig(restConfig), "")
+	k, err := kube.NewClient(kube.NewClientConfigForRestConfig(restConfig))
 	if err != nil {
 		log.Fatal(err)
 	}
-	rc, err := k.UtilFactory().RESTClient()
+	cfg := k.RESTConfig()
+	cfg.APIPath = "/apis/apps"
+	rc, err := rest.RESTClientFor(cfg)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -61,12 +49,6 @@ func main() {
 		client: rc,
 	}
 	_ = c
-	r, err := Get[wrapper](&c, "coredns", "kube-system", metav1.GetOptions{})
+	r, err := Get[appsv1.Deployment](&c, "coredns", "kube-system", metav1.GetOptions{})
 	fmt.Println(r, err)
-	fmt.Println(test("a"))
-	fmt.Println(test(2))
-}
-
-type wrapper struct {
-	*appsv1.Deployment
 }
