88805b1663d7c1f17d6cbff3efe323077fc057a0
John Howard <howardjohn@google.com>
2022-08-28 08:49:43 -0700

    Stub for fair

--- pkg/sel/util.go
+++ pkg/sel/util.go
@@ -20,6 +20,8 @@ type ChannelType interface {
 	apply(t any)
 }
 
+type Strategy func(f ...ChannelType) bool
+
 func Random(f ...ChannelType) bool {
 	cases := make([]reflect.SelectCase, 0, len(f))
 	for _, c := range f {
@@ -67,4 +69,46 @@ func Prioritized(f ...ChannelType) bool {
 	c := f[chosen]
 	c.apply(v.Interface())
 	return true
+}
+
+func Repeat(strategy Strategy, f ...ChannelType) {
+	for {
+		if !strategy(f...) {
+			return
+		}
+	}
+}
+
+func RepeatFair(f ...ChannelType) {
+	cases := make([]reflect.SelectCase, 0, len(f)+1)
+	cases = append(cases, reflect.SelectCase{Dir: reflect.SelectDefault})
+	for _, c := range f {
+		cases = append(cases, reflect.SelectCase{
+			Dir:  reflect.SelectRecv,
+			Chan: c.channel(),
+		})
+	}
+	for {
+		for i := 1; i < len(cases); i++ {
+			chosen, v, ok := reflect.Select(cases[0:i])
+			if chosen == 0 {
+				// default; continue
+				continue
+			}
+			if !ok {
+				return false
+			}
+			c := f[chosen-1]
+			c.apply(v.Interface())
+			return true
+		}
+		// None were ready, try randomly now
+		chosen, v, ok := reflect.Select(cases[1:])
+		if !ok {
+			return false
+		}
+		c := f[chosen]
+		c.apply(v.Interface())
+		return true
+	}
 }
--- pkg/sel/util_test.go
+++ pkg/sel/util_test.go
@@ -7,7 +7,7 @@ import (
 )
 
 func TestRandom(t *testing.T) {
-	e := Expectation(t, true)
+	e := Expectation(t)
 	c1 := make(chan int)
 	c2 := make(chan string)
 	go func() {
@@ -15,40 +15,51 @@ func TestRandom(t *testing.T) {
 	}()
 	sel.Random(
 		sel.Channel[int]{c1, func(i int) {
-			Expectation(t, false).Apply()
+			t.Fatalf("unexpected esnd")
 		}},
 		sel.Channel[string]{c2, func(i string) {
-			e.Apply()
+			e.Call()
 		}},
 	)
 }
 
 type Must struct {
-	sent bool
-	expect bool
+	calls    int
+	expected int
+	t        *testing.T
 }
 
-func Expectation(t *testing.T, must bool) *Must {
-	m := &Must{sent: false, expect: must}
-	if must {
-		t.Cleanup(func() {
-			if !m.sent {
-				t.Fatalf("never sent")
-			}
-		})
-	}
+func Expectation(t *testing.T) *Must {
+	m := &Must{calls: 0, expected: 1, t: t}
+	t.Cleanup(func() {
+		m.Assert()
+	})
 	return m
 }
 
-func (m *Must) Apply() {
-	if m.sent {
-		panic("Apply() called twice")
+func (m *Must) Reset() *Must {
+	m.calls = 0
+	return m
+}
+
+func (m *Must) Expect(calls int) *Must {
+	m.expected = calls
+	return m
+}
+
+func (m *Must) Call() *Must {
+	m.calls++
+	return m
+}
+
+func (m *Must) Assert() {
+	if m.calls != m.expected {
+		m.t.Fatalf("expected %v calls, got %v", m.expected, m.calls)
 	}
-	m.sent = true
 }
 
 func TestPrioritized(t *testing.T) {
-	e := Expectation(t, true)
+	e := Expectation(t)
 	c1 := make(chan int, 1)
 	c2 := make(chan string, 1)
 	unsent := make(chan string, 1)
@@ -56,14 +67,45 @@ func TestPrioritized(t *testing.T) {
 	c1 <- 1
 	chans := []sel.ChannelType{
 		sel.Channel[string]{unsent, func(i string) {
-			Expectation(t, false).Apply()
+			t.Fatalf("unexpected send")
 		}},
 		sel.Channel[int]{c1, func(i int) {
-			e.Apply()
+			e.Call()
 		}},
 		sel.Channel[string]{c2, func(i string) {
-			Expectation(t, false).Apply()
+			t.Fatalf("unexpected send")
 		}},
 	}
 	sel.Prioritized(chans...)
+}
+
+func TestRepeat(t *testing.T) {
+	c1 := make(chan int, 1)
+	e1, e2 := Expectation(t).Expect(3), Expectation(t).Expect(1)
+	c2 := make(chan string, 1)
+	unsent := make(chan string, 1)
+
+	chans := []sel.ChannelType{
+		sel.Channel[string]{unsent, func(i string) {
+			t.Fatalf("unexpected send")
+		}},
+		sel.Channel[int]{c1, func(i int) {
+			e1.Call()
+		}},
+		sel.Channel[string]{c2, func(i string) {
+			e2.Call()
+		}},
+	}
+	done := make(chan struct{})
+	go func() {
+		sel.Repeat(sel.Random, chans...)
+		close(done)
+	}()
+
+	c1 <- 1
+	c1 <- 1
+	c1 <- 1
+	c2 <- "s"
+	close(c2)
+	<-done
 }
