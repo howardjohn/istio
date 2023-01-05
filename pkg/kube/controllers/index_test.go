4a89548a4cf8fc8ac38c69173e61b633abd000dc
John Howard <howardjohn@google.com>
2023-01-05 13:16:41 -0800

    new idea

--- pkg/kube/controllers/index_test.go
+++ pkg/kube/controllers/index_test.go
@@ -16,12 +16,15 @@ package controllers
 
 import (
 	"context"
+	"sync"
 	"testing"
 	"time"
 
+	"golang.org/x/exp/maps"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/client-go/tools/cache"
 
 	"istio.io/istio/pkg/kube"
 	"istio.io/istio/pkg/test"
@@ -138,4 +141,150 @@ func TestIndex(t *testing.T) {
 	c.Kube().CoreV1().Pods("ns").Delete(context.Background(), pod1Alt.Name, metav1.DeleteOptions{})
 	assertIndex(k1, pod3) // Shouldn't impact others
 	assertIndex(keyNew)   // but should be removed
+}
+
+type PodInfo struct {
+	Name   string
+	Labels map[string]string
+}
+
+func (p PodInfo) Equals(k PodInfo) bool {
+	return maps.Equal(p.Labels, k.Labels) && p.Name == k.Name
+}
+
+func (p PodInfo) Key() string {
+	return p.Name
+}
+
+type ServiceInfo struct {
+	Name     string
+	Selector map[string]string
+}
+
+func (p ServiceInfo) Equals(k ServiceInfo) bool {
+	return maps.Equal(p.Selector, k.Selector) && p.Name == k.Name
+}
+
+func (p ServiceInfo) Key() string {
+	return p.Name
+}
+
+func TestDependency(t *testing.T) {
+	c := kube.NewFakeClient()
+	podInf := c.KubeInformer().Core().V1().Pods().Informer()
+	serviceInf := c.KubeInformer().Core().V1().Services().Informer()
+	// I now have a stream for PodInfo
+	podInfo := Subscribe[*corev1.Pod, PodInfo](
+		podInf,
+		func(i *corev1.Pod) PodInfo {
+			return PodInfo{Name: i.Name, Labels: i.Labels}
+		})
+	// I now have a stream for ServiceInfo
+	svcInfo := Subscribe[*corev1.Service, ServiceInfo](
+		serviceInf,
+		func(i *corev1.Service) ServiceInfo {
+			return ServiceInfo{Name: i.Name, Selector: i.Spec.Selector}
+		})
+
+	c.RunAndWait(test.NewStop(t))
+	podInfo.Register(func(info PodInfo) {
+		log.Errorf("howardjohn: handle pod %v", info)
+		for _, svc := range svcInfo.List() {
+			RegisterDependency(svcInfo, svc, info)
+
+		}
+	})
+	svcInfo.Register(func(info ServiceInfo) {
+		log.Errorf("howardjohn: handle server %v", info)
+	})
+
+	c.Kube().CoreV1().Pods("default").Create(context.Background(), &corev1.Pod{
+		ObjectMeta: metav1.ObjectMeta{Name: "pod1", Labels: map[string]string{"app": "bar"}},
+	}, metav1.CreateOptions{})
+	c.Kube().CoreV1().Services("default").Create(context.Background(), &corev1.Service{
+		ObjectMeta: metav1.ObjectMeta{Name: "pod1"},
+		Spec: corev1.ServiceSpec{Selector: map[string]string{"app": "bar"}},
+	}, metav1.CreateOptions{})
+	c.Kube().CoreV1().Services("default").Update(context.Background(), &corev1.Service{
+		ObjectMeta: metav1.ObjectMeta{Name: "pod1"},
+		Spec: corev1.ServiceSpec{Selector: map[string]string{"app": "bar2"}},
+	}, metav1.UpdateOptions{})
+	c.Kube().CoreV1().Services("default").Update(context.Background(), &corev1.Service{
+		ObjectMeta: metav1.ObjectMeta{Name: "pod1", Labels: map[string]string{"ignore": "me"}},
+		Spec: corev1.ServiceSpec{Selector: map[string]string{"app": "bar2"}},
+	}, metav1.UpdateOptions{})
+	time.Sleep(time.Second)
+}
+
+func RegisterDependency[I Equaler[I], O Equaler[O]](hi *Handle[I], input I, ho *Handle[O], output O) {
+
+
+}
+
+type Equaler[K any] interface {
+	Equals(k K) bool
+}
+
+type Handle[O Equaler[O]] struct {
+	handlers []func(O)
+	objects map[string]O
+	mu sync.RWMutex
+}
+
+func (h *Handle[O]) Register(f func(O)) {
+	h.handlers = append(h.handlers, f)
+}
+
+func (h *Handle[O]) Handle(conv O) {
+	for _, hh := range h.handlers {
+		hh(conv)
+	}
+}
+
+func (h *Handle[O]) List() []O {
+	return maps.Values(h.objects)
+}
+
+func Subscribe[I Object, O Equaler[O]](informer HandleInformer, convert func(i I) O) *Handle[O] {
+	h := &Handle[O]{
+		objects: make(map[string]O),
+		mu: sync.RWMutex{},
+	}
+
+	addObj := func(obj any) {
+		i := Extract[I](obj)
+		key, _ := cache.MetaNamespaceKeyFunc(obj)
+		conv := convert(i)
+		updated := !conv.Equals(h.objects[key])
+		h.objects[key] = conv
+		if updated {
+			h.Handle(conv)
+		}
+	}
+	deleteObj := func(obj any) {
+		key, _ := cache.MetaNamespaceKeyFunc(obj)
+		old := h.objects[key]
+		delete(h.objects, key)
+		h.Handle(old)
+	}
+	handler := cache.ResourceEventHandlerFuncs{
+		AddFunc: func(obj any) {
+			h.mu.Lock()
+			defer h.mu.Unlock()
+			addObj(obj)
+		},
+		UpdateFunc: func(oldObj, newObj any) {
+			h.mu.Lock()
+			defer h.mu.Unlock()
+			deleteObj(oldObj)
+			addObj(newObj)
+		},
+		DeleteFunc: func(obj any) {
+			h.mu.Lock()
+			defer h.mu.Unlock()
+			deleteObj(obj)
+		},
+	}
+	informer.AddEventHandler(handler)
+	return h
 }
