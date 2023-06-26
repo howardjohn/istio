c6026c544f748d01a71e648ada0a0f08f926b9df
John Howard <>
2023-06-26 11:38:55 -0700

    Update BASE_VERSION to 1.2.3-2023-06-26T11-38-54

--- Makefile.core.mk
+++ Makefile.core.mk
@@ -49,7 +49,7 @@ endif
 export VERSION
 
 # Base version of Istio image to use
-BASE_VERSION ?= master-2023-06-15T19-01-36
+BASE_VERSION ?= 1.2.3-2023-06-26T11-38-54
 ISTIO_BASE_REGISTRY ?= gcr.io/istio-release
 
 export GO111MODULE ?= on
