8497b508e628e5377c9511f9da579020b6edde83
John Howard <john.howard@solo.io>
2024-12-30 12:09:37 -0800

    experimental: manual marshal
    ```
    goos: linux
    goarch: amd64
    pkg: istio.io/istio/pkg/util/protomarshal
    cpu: AMD Ryzen 7 PRO 7840U w/ Radeon 780M Graphics
                          │  /tmp/bench  │
                          │    sec/op    │
    Encoding/vt-16          2.918µ ± ∞ ¹
    Encoding/legacy-16      11.64µ ± ∞ ¹
    Encoding/optimized-16   134.7n ± ∞ ¹
    geomean                 1.660µ
    ¹ need >= 6 samples for confidence interval at level 0.95
    
                          │ /tmp/bench  │
                          │    B/op     │
    Encoding/vt-16          16.00 ± ∞ ¹
    Encoding/legacy-16      16.00 ± ∞ ¹
    Encoding/optimized-16   128.0 ± ∞ ¹
    geomean                 32.00
    ¹ need >= 6 samples for confidence interval at level 0.95
    
                          │ /tmp/bench  │
                          │  allocs/op  │
    Encoding/vt-16          1.000 ± ∞ ¹
    Encoding/legacy-16      1.000 ± ∞ ¹
    Encoding/optimized-16   4.000 ± ∞ ¹
    geomean                 1.587
    ¹ need >= 6 samples for confidence interval at level 0.95
    ```

--- pkg/util/protomarshal/grpc.go
+++ pkg/util/protomarshal/grpc.go
@@ -1,6 +1,8 @@
 package protomarshal
 
 import (
+	vtanypb "github.com/planetscale/vtprotobuf/types/known/anypb"
+	discoveryv3 "github.com/envoyproxy/go-control-plane/envoy/service/discovery/v3"
 	"google.golang.org/grpc/encoding"
 	"google.golang.org/grpc/mem"
 	"istio.io/istio/pilot/pkg/features"
@@ -45,21 +47,27 @@ func (Codec) Name() string { return Name }
 var defaultBufferPool = mem.DefaultBufferPool()
 
 func (c *Codec) Marshal(v any) (mem.BufferSlice, error) {
-	if m, ok := v.(vtprotoMessage); ok {
-		size := m.SizeVT()
-		if mem.IsBelowBufferPoolingThreshold(size) {
-			buf := make([]byte, size)
-			if _, err := m.MarshalToSizedBufferVTStrict(buf[:size]); err != nil {
+
+	if features.EnableVtprotobuf {
+		if dr, ok := v.(*discoveryv3.DeltaDiscoveryResponse); ok {
+			return EncodeDiscoveryResponse(dr)
+		}
+		if m, ok := v.(vtprotoMessage); ok {
+			size := m.SizeVT()
+			if mem.IsBelowBufferPoolingThreshold(size) {
+				buf := make([]byte, size)
+				if _, err := m.MarshalToSizedBufferVTStrict(buf[:size]); err != nil {
+					return nil, err
+				}
+				return mem.BufferSlice{mem.SliceBuffer(buf)}, nil
+			}
+			buf := defaultBufferPool.Get(size)
+			if _, err := m.MarshalToSizedBufferVTStrict((*buf)[:size]); err != nil {
+				defaultBufferPool.Put(buf)
 				return nil, err
 			}
-			return mem.BufferSlice{mem.SliceBuffer(buf)}, nil
+			return mem.BufferSlice{mem.NewBuffer(buf, defaultBufferPool)}, nil
 		}
-		buf := defaultBufferPool.Get(size)
-		if _, err := m.MarshalToSizedBufferVTStrict((*buf)[:size]); err != nil {
-			defaultBufferPool.Put(buf)
-			return nil, err
-		}
-		return mem.BufferSlice{mem.NewBuffer(buf, defaultBufferPool)}, nil
 	}
 
 	return c.fallback.Marshal(v)
@@ -77,4 +85,72 @@ func init() {
 			fallback: encoding.GetCodecV2("proto"),
 		})
 	}
+}
+
+func EncodeDiscoveryResponse(t *discoveryv3.DeltaDiscoveryResponse) (mem.BufferSlice, error) {
+	res := t.Resources
+	t.Resources = nil
+	bs := make(mem.BufferSlice, 0, 2*len(res)+1)
+	buf, err := t.MarshalVTStrict()
+	if err != nil {
+		return nil, err
+	}
+	bs = append(bs, mem.NewBuffer(&buf, nil))
+	for iNdEx := len(res) - 1; iNdEx >= 0; iNdEx-- {
+		r := res[iNdEx]
+		// TODO: split the name from the value as well
+		buf1, buf2, err := encodeResource(r)
+		if err != nil {
+			return nil, err
+		}
+		bs = append(bs, mem.NewBuffer(&buf1, nil), mem.NewBuffer(&buf2, nil))
+	}
+	return bs, nil
+}
+
+func encodeResource(r *discoveryv3.Resource) ([]byte, []byte, error) {
+	hdr := make([]byte, 0, len(r.Name) + len(r.Resource.TypeUrl) + 2 + 20)
+	// Tag for resource in DeltaDiscoveryResponse
+	hdr = append(hdr, 0x12)
+	hdr = appendVarint(hdr, uint64(r.SizeVT()))
+
+	// Name in DeltaDiscoveryResponse
+	hdr = append(hdr, 0x1a)
+	hdr = appendVarint(hdr, uint64(len(r.Name)))
+	hdr = append(hdr, r.Name...)
+
+	// Now the Resource...
+	a := (*vtanypb.Any)(r.Resource)
+	hdr = append(hdr, 0x12)
+	hdr = appendVarint(hdr, uint64(a.SizeVT()))
+
+	hdr = append(hdr, 0xa)
+	hdr = appendVarint(hdr, uint64(len(a.TypeUrl)))
+	hdr = append(hdr, a.TypeUrl...)
+
+	hdr = append(hdr, 0x12)
+	hdr = appendVarint(hdr, uint64(len(a.Value)))
+	return hdr, a.Value, nil
+}
+
+func encodeVarint(tag byte, x uint64) []byte {
+	var buf [11]byte
+	buf[0] = tag
+	var n int
+	for n = 0; x > 127; n++ {
+		buf[n+1] = 0x80 | uint8(x&0x7F)
+		x >>= 7
+	}
+	buf[n+1] = uint8(x)
+	n++
+	return buf[0 : n+1]
+}
+
+func appendVarint(b []byte, x uint64) []byte {
+	for x >= 128 {
+		b = append(b, byte(x)|0x80)
+		x >>= 7
+	}
+	b = append(b, byte(x))
+	return b
 }
